fbc3de9cfcd1bed8482ccab1cc8e4c4be8e4af5d
Svyatoslav <svyatoslavmartiian@gmail.com>
2025-12-12 17:59:31 +0200

    d

---
+++ d.py
@@ -0,0 +1,3 @@
+print("1\nTo be\nor not\nto be")
+print(" 2\n\"Life is what happens\n\twhen \n\tyou're busy making other plans\" \n\tJohn Lennon")
+print(" 3\n\"The best way to predict the future \n\tis to \n\t\tinvent it\" \n\t\t\tAlan Kay")
