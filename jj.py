cc9cc8795434259350be2171b85c9625cc1300b1
Svyatoslav <svyatoslavmartiian@gmail.com>
2026-01-10 01:03:55 +0200

    HW

--- jj.py
+++ jj.py
@@ -45,7 +45,7 @@ elif currency == "UAN":
 #Завдання 6 
 
 number7 = float(input("Ведіть час у секундах "))
-number8 = str(input("Ведіть скільки залишилось часу в (секундах(A), хвилинах (B) або годинах (C))"))
+number8 = str(input("Оберіть одиницю вимірювання часу (секунда(A), хвилина (B) або година (C))"))
 if number8 == "A":
       print(f"Залишок часу до опівночі в секундах {86400 - number7}")
 elif number8 == "B":
@@ -54,7 +54,52 @@ elif number8 == "C":
       print(f"Залишок часу до опівночі в хвилинах {(86400 - number7) / 60 / 60}")
 
 #Завдання 7
+PI = 3.14
+number9 = float(input("Ведіть діаметр кола "))
+text = str(input("Оберіть що порахувати (площу(A), периметр (B))"))
+if text == "A":
+      print(f"Площа кола дорівнює {PI * (number9 * number9)}")
+elif text == "B":
+      print(f"Периметр кола дорівнює {PI * number9}")
+#Завдання 8
+number10 = float(input("Ведіть розмір файлу "))
+number11 = float(input("Ведіть швидкість інтернету" ))
+text2 = str(input("Оберіть одиницю вимірювання часу (секунда(A), хвилина(B) або годинa(C) "))
+if text2 == "A":
+      print(f"Ваш файл завантажиться через {(number10 * 8589934592) / number11} секунд")
+elif text2 == "B": 
+      print(f"Ваш файл завантажиться через {(number10 * 8589934592) / number11 / 60} хвилин")
+elif text2 == "C": 
+     print(f"Ваш файл завантажиться через {(number10 * 8589934592) / number11 / 60 / 60} годин")
+     
+#Завдання 9 
 
+number12 = float(input("Ведіть кількість годин "))
+if number12 >= 0 and number12 < 6:
+    print("Good Night")
+elif number12 >= 6 and number12 < 13:
+      print("Good Morning")
+elif number12 >=13 and number12 < 17:
+      print("Good Day")
+elif number12 >=17 and number12 < 0:
+      print("Good Evening")
+
+#Завдання 10
+
+number13 = float(input("Ведіть поточну температуру "))
+if number13 < -10:
+    print("Дуже холодно")
+elif number12 >= -10 and number12 < 0:
+      print("Холодно")
+elif number12 >=0 and number12 < 15:
+      print("Прохолодно")
+elif number12 >=15 and number13 < 25:
+      print("Тепло")
+elif number13 > 25:
+      print("Спекотно")
+
+
+ 
       
 
 
