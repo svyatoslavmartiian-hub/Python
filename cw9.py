7cebd4fe1f3386cff8f6a9b267e5fc2cb98249af
Svyatoslav <svyatoslavmartiian@gmail.com>
2026-01-23 19:32:55 +0200

    cw9

---
+++ cw9.py
@@ -0,0 +1,62 @@
+# n1
+# num1 = int(input("Ведіть перше число: "))
+# num2 = int(input("Ведіть друге число: "))
+# num3 = num1
+# while num3 <= num2:
+#     print(f"{num3}")
+#     num3 +=1
+
+# n2
+
+# num1 = int(input("Ведіть перше число: "))
+# num2 = int(input("Ведіть друге число: "))
+# num3 = num1
+# while num3 <= num2:
+#     if num3 % 2 == 1:
+#         print(f"{num3}")
+#     num3 +=1 
+
+# n3
+
+# num1 = int(input("Ведіть перше число: "))
+# num2 = int(input("Ведіть друге число: "))
+# num3 = num2
+# while num3 >= num1:
+#     if num3 % 2 == 0:
+#         print(f"{num3}")
+#     num3 -=1 
+
+# n4
+
+# num1 = int(input("Ведіть перше число: "))
+# num2 = int(input("Ведіть друге число: "))
+# num3 = int(input("Ведіть порядок діапазона (1 - зростаючий, 2 - спадаючий) "))
+# num4 = num2
+# while num4 >= num1:
+#     if num3 == 2:
+#         print(f"{num4}")
+#     num4 -=1 
+
+# num5 = int(input("Ведіть перше число: "))
+# num6 = int(input("Ведіть друге число: "))
+# num7 = int(input("Ведіть порядок діапазона (1 - зростаючий, 2 - спадаючий) "))
+# num8 = num5
+# while num8 <= num2:
+#     if num7 == 1:
+#         print(f"{num8}")
+#     num8 +=1 
+
+# n5
+
+
+
+
+
+   
+
+
+
+
+
+ 
+
