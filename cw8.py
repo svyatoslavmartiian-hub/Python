ff5231d72014a4043aa7403b9240854ace512f53
Svyatoslav <svyatoslavmartiian@gmail.com>
2026-01-29 22:40:19 +0200

    hw9.py

--- cw8.py
+++ cw8.py
@@ -14,45 +14,45 @@
 # else:
 #     print("Числа однакові")
 
-needed_potates = int(input("Ведіть ількість потрібної картоплі"))
+# needed_potates = int(input("Ведіть ількість потрібної картоплі"))
 
-peeled_potates = 0
+# peeled_potates = 0
 
-while peeled_potates < needed_potates:
-    print("Чистимо картоплю")
-    is_rotten = input("Картопля гнила?")
-    if is_rotten == "так":
-        print("Викидаємо!")
-        continue
-    print("Чистимо картоплю...")
-    print("готово")
-    peeled_potates +=1
-
-
+# while peeled_potates < needed_potates:
+#     print("Чистимо картоплю")
+#     is_rotten = input("Картопля гнила?")
+#     if is_rotten == "так":
+#         print("Викидаємо!")
+#         continue
+#     print("Чистимо картоплю...")
+#     print("готово")
+#     peeled_potates +=1
 
 
-print(f"Почистили {peeled_potates} штук картоплі")
 
 
-# while True:
-#     num1 = float(input("Ведіть перше число"))
-#     num2 = float(input("Ведіть друге число"))
-#     action = input("Оберіть операцію (+, -, *, /): ")
+# print(f"Почистили {peeled_potates} штук картоплі")
 
-#     match action:
-#             case "+":
-#                 print(f"{num1} + {num2} = {num1 + num2}")
-#             case "-":
-#                 print(f"{num1} - {num2} = {num1 - num2}")
-#             case "*":
-#                 print(f"{num1} * {num2} = {num1 * num2}")
-#             case "/":
-#                 if num2 == 0:
-#                      print("Неможна ділити на нуль")
-#                 else:
-#                     print(f"{num1} / {num2} = {num1 / num2}")
-#             case _:
-#                 print("Некоректна операція!")
-#     q = input("Ведіть q щоб завершити.Натисніть Enter щоб продовжити.")
-#     if q == "q":
-#          break
+
+while True:
+    num1 = float(input("Ведіть перше число"))
+    num2 = float(input("Ведіть друге число"))
+    action = input("Оберіть операцію (+, -, *, /): ")
+
+    match action:
+            case "+":
+                print(f"{num1} + {num2} = {num1 + num2}")
+            case "-":
+                print(f"{num1} - {num2} = {num1 - num2}")
+            case "*":
+                print(f"{num1} * {num2} = {num1 * num2}")
+            case "/":
+                if num2 == 0:
+                     print("Неможна ділити на нуль")
+                else:
+                    print(f"{num1} / {num2} = {num1 / num2}")
+            case _:
+                print("Некоректна операція!")
+    q = input("Ведіть q щоб завершити.Натисніть Enter щоб продовжити.")
+    if q == "q":
+         break
