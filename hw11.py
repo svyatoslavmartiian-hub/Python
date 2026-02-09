f33f8f1e5e57cf2cc56245d884532b31db7cdb55
Svyatoslav <svyatoslavmartiian@gmail.com>
2026-02-09 17:55:22 +0200

    hw11

--- hw11.py
+++ hw11.py
@@ -75,26 +75,80 @@
 
 
 
-left = int(input('Введіть ліву межу діапазону: '))
-right = int(input('Введіть праву межу діапазону: '))
-kr = int(input("Ведіть інтервал: "))
-intr = str(input("Виберіть у якому порядку вивести числа(1-Спадаючий, 2-Зростаючий): "))
 
-if left > right:
-    left, right = right, left
+# start = int(input("Введи початок діапазону: "))
+# end = int(input("Введи кінець діапазону: "))
+# step = int(input("Введи крок: "))
 
-counter = left
-
-countMultiply5 = 0
-
-print("Усі числа діапазону: ")
-while counter <= right:
-    if intr == 1:
-        print(counter, end=' ')
-    counter += kr
+# num1 = input("Порядок (1 - зростаючий, 2 - спадаючий): ")
+# if start > end:
+#     start, end = end,start
 
 
-    
+# if num1 == "1":
+#     num = start
+#     while num <= end:
+#         print(num)
+#         num += step
+
+
+# elif num1 == "2":
+#     num = end
+#     while num >= start:
+#         print(num)
+#         num -= step
+
+# else:
+#     print("Помилка: невірний порядок!")
+
+
+
+
+
+# num1 = int(input("Введи перше число: "))
+# num2 = int(input("Введи друге число: "))
+
+
+# if num1 > num2:
+#     num1, num2 = num2, num1
+
+# product = 1
+# found = 0
+# num3 = num1
+
+# while num3 <= num2:
+#     if num3 % 4 == 0 and num3 % 6 != 0:
+#         product *= num3
+#         found = 1
+#     num3 += 1
+
+# if found:
+#     print("Добуток чисел =", product)
+# else:
+#     print("Немає чисел, що діляться на 4 і не діляться на 6")
+
+
+
+
+
+
+# num1 = int(input("Введи число: "))
+# num2 = int(input("Введи степінь: "))
+
+# result = 1
+# num3 = 0
+
+# while num3 < num2:
+#     result *= num1
+#     num3 += 1
+
+# print("Результат:", result)
+
+
+
+
+
+
 
 
    
