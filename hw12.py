5eb9e5c16f0ad7f4855268404951d61858f6fd3c
Svyatoslav <svyatoslavmartiian@gmail.com>
2026-02-11 22:08:59 +0200

    hw12

---
+++ hw12.py
@@ -0,0 +1,234 @@
+# # width = int(input("Введите ширину: "))
+# # height = int(input("Введите высоту: "))
+# symbol = input("Введите символ для рисования: ")
+
+# for i in range(5):          # идём по строкам
+#     for j in range(3):       # идём по колонкам
+#         print(symbol, end=' ')   # печатаем символ с пробелом и остаёмся на той же строке
+#     print()                      # переходим на следующую строку
+
+n = 12  # розмір (непарне число)
+print("Фігура 1:")
+
+for i in range(n):
+    for j in range(n):
+        if i <= j and i + j >= n - 1:
+            print("*", end=" ")
+        elif i >= j and i + j <= n - 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+print("Фігура 2:")
+
+for i in range(n):
+    for j in range(n):
+        if i >= j and i + j <= n - 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+print("Фігура 3:")
+
+
+for i in range(n):
+    for j in range(n):
+        if i <= j and i + j >= n - 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+print("Фігура 4:")
+
+
+for i in range(n):
+    for j in range(n):
+        if j <= n - i - 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+print("Фігура 5:")
+ 
+for i in range(n):
+    for j in range(n):
+        if j >= n - i + 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+print("Фігура 6:")
+
+for i in range(n):
+    for j in range(n):
+        if j >= i:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+print("Фігура 7:")
+
+
+for i in range(n):
+    for j in range(n):
+        if j <= i:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+print("Фігура 8:")
+
+
+t = 7
+for i in range(t, 0, -1):
+    for j in range(t - i):
+        print(" ", end=" ")
+    for j in range(2 * i - 1):
+        print("*", end=" ")
+    print()
+
+print("Фігура 9:")
+  
+
+
+
+for i in range(1, t + 1):
+    for j in range(t - i):
+        print(" ", end=" ")
+    for j in range(2 * i - 1):
+        print("*", end=" ")
+    print()
+print("Фігура 10:")
+
+
+
+for i in range(t, 0, -1):
+    for j in range(t - i):
+        print(" ", end=" ")
+    for j in range(2 * i - 1):
+        print("*", end=" ")
+    print()
+
+
+for i in range(2, t + 1):
+    for j in range(t - i):
+        print(" ", end=" ")
+    for j in range(2 * i - 1):
+        print("*", end=" ")
+    print()
+
+
+intt = int(input("Ведіть розмір фігури: "))
+intt2 = int(input("Ведіть символ яким намалювати фігури: "))
+
+
+print("Фігура 1:")
+
+for i in range(intt):
+    for j in range(intt):
+        if i <= j and i + j >= intt - 1:
+            print(intt2, end=" ")
+        elif i >= j and i + j <= intt - 1:
+            print(intt2, end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+print("Фігура 2:")
+
+for i in range(intt):
+    for j in range(intt):
+        if i >= j and i + j <= intt - 1:
+            print(intt2, end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+print("Фігура 3:")
+
+
+for i in range(intt):
+    for j in range(intt):
+        if i <= j and i + j >= intt - 1:
+            print(intt2, end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+print("Фігура 4:")
+
+
+for i in range(intt):
+    for j in range(intt):
+        if j <= intt - i - 1:
+            print(intt2, end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+print("Фігура 5:")
+ 
+for i in range(intt):
+    for j in range(intt):
+        if j >= intt - i + 1:
+            print(intt2, end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+print("Фігура 6:")
+
+for i in range(intt):
+    for j in range(intt):
+        if j >= i:
+            print(intt2, end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+print("Фігура 7:")
+
+
+for i in range(intt):
+    for j in range(intt):
+        if j <= i:
+            print(intt2, end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+print("Фігура 8:")
+
+
+t = 7
+for i in range(intt, 0, -1):
+    for j in range(intt - i):
+        print(" ", end=" ")
+    for j in range(2 * i - 1):
+        print(intt2, end=" ")
+    print()
+
+print("Фігура 9:")
+  
+
+
+
+for i in range(1, intt + 1):
+    for j in range(intt - i):
+        print(" ", end=" ")
+    for j in range(2 * i - 1):
+        print(intt2, end=" ")
+    print()
+print("Фігура 10:")
+
+
+
+for i in range(intt, 0, -1):
+    for j in range(intt - i):
+        print(" ", end=" ")
+    for j in range(2 * i - 1):
+        print(intt2, end=" ")
+    print()
+
+
+for i in range(2, intt + 1):
+    for j in range(intt - i):
+        print(" ", end=" ")
+    for j in range(2 * i - 1):
+        print(intt2, end=" ")
+    print()
